e5f11e87bc72da75e381ce095c5ec4347028dc42
Diganta Mandal <diganta@emerchantpay.com>
2014-06-03 15:18:53 +0300

    initial commit

--- lib/generators/salesforce/config/config_generator.rb
+++ lib/generators/salesforce/config/config_generator.rb
@@ -1,12 +1,14 @@
 module Salesforce
-  module Generators
-    class ConfigGenerator < Rails::Generators::Base
-      desc 'Creates a Salesforce-Rails gem configuration file at config/salesforce.yml'
+  module Rails
+    module Generators
+      class ConfigGenerator < Rails::Generators::Base
+        desc 'Creates a Salesforce-Rails gem configuration file at config/salesforce.yml'
 
-      source_root File.expand_path("../templates", __FILE__)
+        source_root File.expand_path("../templates", __FILE__)
 
-      def create_config_file
-        template 'salesforce.yml', File.join('config', 'salesforce.yml')
+        def create_config_file
+          template 'salesforce.yml', File.join('config', 'salesforce.yml')
+        end
       end
     end
   end
