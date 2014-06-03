faaca3a833b051d58a8915e6b0309495a8cbeeb2
Diganta Mandal <diganta@emerchantpay.com>
2014-06-03 17:28:15 +0300

    initial commit

--- lib/generators/salesforce/calendar/install/install_generator.rb
+++ lib/generators/salesforce/calendar/install/install_generator.rb
@@ -2,26 +2,43 @@ require 'rails'
 
 # Supply generator for Rails 3.0.x or if asset pipeline is not enabled
 #if ::Rails.version < "3.1" || !::Rails.application.config.assets.enabled
-  module Salesforce
-    module Calendar
-      module Generators
-        class InstallGenerator < ::Rails::Generators::Base
-          desc "This generator installs jQuery Fullcalendar"
-          source_root File.expand_path('../../../../../../vendor/assets', __FILE__)
-
-          def copy_javascripts
-            template 'javascripts/jquery-fullcalendar.js', File.join('public/javascripts', 'jquery-fullcalendar.js')
-            template 'javascripts/jquery-qtip.js', File.join('public/javascripts', 'jquery-qtip.js')
-          end
-
-          def copy_stylesheets
-            template 'stylesheets/jquery-fullcalendar.css', File.join('public/stylesheets', 'jquery-fullcalendar.css')
-            template 'stylesheets/jquery-fullcalendar-print.css', File.join('public/stylesheets', 'jquery-fullcalendar-print.css')
-          end
+module Salesforce
+  module Calendar
+    module Generators
+      class InstallGenerator < ::Rails::Generators::Base
+        desc "This generator installs jQuery Fullcalendar"
+        source_root File.expand_path('../../../../../../vendor/assets', __FILE__)
+        
+        def copy_javascripts
+          template 'javascripts/salesforce/fullcalendar.js', File.join('public/javascripts', 'salesforce/fullcalendar.js')
+          template 'javascripts/salesforce/qtip.js', File.join('public/javascripts', 'salesforce/qtip.js')
         end
+
+        def copy_stylesheets
+          template 'stylesheets/salesforce/fullcalendar.css', File.join('public/stylesheets', 'salesforce/fullcalendar.css')
+          template 'stylesheets/salesforce/fullcalendar.print.css', File.join('public/stylesheets', 'salesforce/fullcalendar.print.css')
+          template 'stylesheets/salesforce/calendar.css', File.join('public/stylesheets', 'salesforce/calendar.css')
+        end
+
+#        def append_asset_pipeline!
+#          application_css = 'app/assets/stylesheets/application.css'
+#          if File.file?(application_css)
+#            insert_into_file application_css, %q{*= require salesforce/fullcalendar
+#*= require salesforce/fullcalendar.print
+#*= require salesforce/calendar#}, :before => "*= require_self"
+#          end
+#
+#          application_js = 'app/assets/javascripts/application.js'
+#          if File.file?(application_js)
+#            insert_into_file application_js, %q{*= require salesforce/fullcalendar
+#*= require salesforce/fullcalendar.print
+#*= require salesforce/calendar#}, :before => "*= require_self"
+#          end
+#        end
       end
     end
   end
+end
 #else
 
 #end
