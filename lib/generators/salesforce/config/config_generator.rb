7a12d87af56bb7a598d88acc37aa63498e2d333e
Diganta Mandal <diganta@emerchantpay.com>
2014-06-03 15:29:32 +0300

    initial commit

--- lib/generators/salesforce/config/config_generator.rb
+++ lib/generators/salesforce/config/config_generator.rb
@@ -1,12 +1,15 @@
+require 'rails'
+
 module Salesforce
   module Generators
     class ConfigGenerator < Rails::Generators::Base
-      desc 'Creates a Salesforce-Rails gem configuration file at config/salesforce.yml'
-
+      desc 'This generator generate a Salesforce-Rails gem configuration file at config/salesforce.yml'
+      #class_option :ui, :type => :boolean, :default => false, :desc => "Include jQueryUI"
       source_root File.expand_path("../templates", __FILE__)
 
-      def create_config_file
-        template 'salesforce.yml', File.join('config', 'salesforce.yml')
+      def generate_config_file
+        say_status('copying', 'salesforce.yml', :green)
+        copy_file 'salesforce.yml', 'config/salesforce.yml'
       end
     end
   end
