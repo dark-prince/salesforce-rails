f19657f34db68ca2b47d79d0c63c3f133652ad4a
Diganta Mandal <diganta@emerchantpay.com>
2014-06-04 11:52:18 +0300

    initial commit

--- lib/salesforce/rails.rb
+++ lib/salesforce/rails.rb
@@ -1,7 +1,7 @@
 require 'yaml' unless defined?(YAML)
 require 'databasedotcom'
 #require 'salesforce/rails/engine' if ::Rails.version >= '3.1'
-#require 'salesforce/rails/railtie'
+require 'salesforce/rails/railtie'
 require 'salesforce/rails/version'
 
 Dir[File.join(File.dirname(__FILE__), 'rails', 'api', '**', '*.rb').to_s].each do |file|
