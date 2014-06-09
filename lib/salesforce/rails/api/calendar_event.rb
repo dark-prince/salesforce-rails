490244c3f3619dd4f4f956ac052c7f5f47db11bd
Diganta Mandal <diganta@emerchantpay.com>
2014-06-09 17:59:30 +0300

    initial commit.

--- lib/salesforce/rails/api/calendar_event.rb
+++ lib/salesforce/rails/api/calendar_event.rb
@@ -7,14 +7,14 @@ module Salesforce
           class << self
             # Returns all calendar events.
             #
-            #   Salesforce::Rails::API::Calendar::Event.all #=> [#<Event @Id="1", ...>, #<Event @Id="2", ...>, #<Event @Id="3", ...>, ...]
+            #   Salesforce::Rails::API::CalendarEvent.all #=> [#<Event @Id="1", ...>, #<Event @Id="2", ...>, #<Event @Id="3", ...>, ...]
             def all
               client.query("SELECT Id, Subject, IsAllDayEvent, StartDateTime, EndDateTime, Description, Owner.Name, ShowAs FROM Event")
             end
 
             # Returns a collection of events of self that match the conditional +where_expr+, which is the WHERE part of a SOQL query.
             #
-            #   Salesforce::Rails::API::Calendar::Event.query("Name = 'test'") #=> [#<Event @Id="1", @Name="test", ...>, #<Event @Id="2", @Name="test", ...>, #<Event @Id="3", @Name="test", ...>, ...]
+            #   Salesforce::Rails::API::CalendarEvent.query("Name = 'test'") #=> [#<Event @Id="1", @Name="test", ...>, #<Event @Id="2", @Name="test", ...>, #<Event @Id="3", @Name="test", ...>, ...]
             def query(where_expr)
               client.query("SELECT Id, Subject, IsAllDayEvent, StartDateTime, EndDateTime, Description, Owner.Name, ShowAs FROM Event WHERE #{where_expr}")
             end
